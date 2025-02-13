8e9f7182bfd7d28f75ff558e5999d04384b4e7d8
M-Sufiyan10 <cutexxxboy90@gmail.com>
2025-02-13 23:09:26 +0500

    UI DONE

--- summarizer.py
+++ summarizer.py
@@ -1,77 +1,55 @@
-# from dotenv import load_dotenv
-# import os
-# from langchain_google_genai import ChatGoogleGenerativeAI
-# from app import transcribe_video, load_video, load_api
-
-# api_key=load_api()['google_api']
-
-# if not api_key:
-#     raise ValueError("Missing GOOGLE_API_KEY. Set it in your .env file.")
-
-# llm = ChatGoogleGenerativeAI(
-#     model="gemini-1.5-pro",
-#     google_api_key=api_key,  
-#     temperature=0,
-#     max_tokens=None,
-#     timeout=None,
-#     max_retries=2
-# )
-# video_url=load_video()
-# text_to_summarize = transcribe_video(video_url)
-# summary_prompt = f"Summarize the following text:\n\n{text_to_summarize}"
-# response = llm.invoke(summary_prompt)
-# print("Summary:", response.content)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-from dotenv import load_dotenv
-import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from app import transcribe_video, fetch_video_url, load_api_keys
 from transcribe import transcribe_audio
 import streamlit as st
 
+
+def setup_LLM_configurations():
+    with st.sidebar:
+        temperature=st.slider("TUNE THE GENIE TEMPERATURE !!!!",min_value=0.0, max_value=1.0,value=0.5,step=0.1)
+        max_tokens=st.slider("TUNE THE GENIE MAX TOKENS !!!!",min_value=100, max_value=1000,value=500,step=100)
+        max_tries=st.slider("TUNE THE GENIE MAX TRIES",min_value=3,max_value=6,value=3,step=1)
+    return {'temperature':temperature,
+            "tokens":max_tokens,
+            "tries":max_tries
+            }
+
+
 def initialize_llm(api_key):
     """Initialize the Google Generative AI model."""
     if not api_key:
         raise ValueError("Missing GOOGLE_API_KEY. Set it in your .env file.")
+    llm_config=setup_LLM_configurations()
     return ChatGoogleGenerativeAI(
-        model="gemini-1.5-pro",
-        google_api_key=api_key,
-        temperature=0.5
-    )
+    model="gemini-1.5-pro",
+    google_api_key=api_key,
+    temperature=llm_config['temperature'],
+    max_tokens=llm_config['tokens'],
+    max_retries=llm_config['tries']
+        )
 
 
 def summarize_text(text, llm):
     """Summarize text using the LLM model."""
-    summary_prompt = f"Summarize the following text:\n\n{text}"
+    summary_prompt = f"Summarize the following text in bullet points :\n\n{text}"
     response = llm.invoke(summary_prompt)
     return response.content
 
 
 def main():
     """Main function to summarize transcribed video content."""
+    st.subheader("TUNEGENIE")
     api_keys = load_api_keys()
     llm = initialize_llm(api_keys["google_api"])
-    
-    query = transcribe_audio()
+    if not st.button("Input your voice"):
+        return False
+    query=transcribe_audio()
     video_url = fetch_video_url(query, api_keys["youtube_api"])
     st.video(video_url)
     text_to_summarize = transcribe_video(video_url)
     summary = summarize_text(text_to_summarize, llm)
+    st.subheader("SUMMARY: ")
     st.write(summary)
-
 
 if __name__ == "__main__":
     main()
--- transcribe.py
+++ transcribe.py
@@ -1,33 +1,31 @@
 import whisper
 import speech_recognition as sr
 import os
-
+import streamlit as st
 def transcribe_audio():
-    print("Loading Whisper model...")
+    st.write("Loading Whisper model...")
     model = whisper.load_model("tiny")  
     recognizer = sr.Recognizer()
 
     with sr.Microphone() as source:
-        print("Adjusting for ambient noise... Please wait.")
+        st.write("Adjusting for ambient noise... Please wait.")
         recognizer.adjust_for_ambient_noise(source, duration=5)
-        print("You can start speaking now...")
+        st.write("You can start speaking now...")
         
         try:
             audio_data = recognizer.listen(source, timeout=10)
-            print("Recording complete. Processing audio...")
+            st.write("GOT IT!!!!!!")
             with open("temp_audio.wav", "wb") as f:
                 f.write(audio_data.get_wav_data())
-            print("Transcribing audio using Whisper...")
             transcription = model.transcribe("temp_audio.wav")
             
-            print("Transcription:")
             transcribed_voice=transcription["text"]
             return transcribed_voice
         
         except sr.WaitTimeoutError:
-            print("No speech detected within the timeout period.")
+            st.write("No speech detected within the timeout period.")
         except Exception as e:
-            print(f"An error occurred: {e}")
+            st.write(f"An error occurred: {e}")
         finally:
             if os.path.exists("temp_audio.wav"):
                 os.remove("temp_audio.wav")
